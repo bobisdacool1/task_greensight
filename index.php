f3c25f2b47fa069529e808037557bde7c2437d1c
aleksei_sinitsyn <sinitsin.aleksey2003@yandex.ru>
2022-03-09 11:29:15 +0500

    Add basic layout

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Greensight</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="row d-flex justify-content-center">
+        <div class="col-6">
+            <form class="mt-5">
+                <div class="mb-3">
+                    <label for="form-name" class="form-label">Имя</label>
+                    <input type="text" class="form-control" id="form-name">
+                </div>
+                <div class="mb-3">
+                    <label for="form-second-name" class="form-label">Фамилия</label>
+                    <input type="text" class="form-control" id="form-second-name">
+                </div>
+                <div class="mb-3">
+                    <label for="form-email" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="form-email">
+                </div>
+                <div class="mb-3">
+                    <label for="form-password" class="form-label">Пароль</label>
+                    <input type="password" class="form-control" id="form-password">
+                </div>
+                <div class="mb-3">
+                    <label for="form-password-confirm" class="form-label">Подтверждение пароля</label>
+                    <input type="password" class="form-control" id="form-password-confirm">
+                </div>
+                <button type="submit" class="btn btn-primary">Отправить</button>
+            </form>
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
