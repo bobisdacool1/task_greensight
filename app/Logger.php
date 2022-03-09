91c725e7476dcd560535417502caf21227185ae5
aleksei_sinitsyn <sinitsin.aleksey2003@yandex.ru>
2022-03-09 14:56:15 +0500

    fix logger

--- app/Logger.php
+++ app/Logger.php
@@ -7,6 +7,6 @@ class Logger
         $currentDate = date("Y-m-d H:i:s");
         $data = json_encode($data, JSON_UNESCAPED_UNICODE);
 
-        file_put_contents($_SERVER["DOCUMENT_ROOT"] . "/logs/log.log", $currentDate . $data . " | " . "\n", FILE_APPEND);
+        file_put_contents($_SERVER["DOCUMENT_ROOT"] . "/logs/log.log", $currentDate . " | " . $data . "\n", FILE_APPEND);
     }
 }
