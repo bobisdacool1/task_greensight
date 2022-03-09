f7ed703fa435f0edf8d70d698931356e3a357fea
aleksei_sinitsyn <sinitsin.aleksey2003@yandex.ru>
2022-03-09 14:48:27 +0500

    add try catch block

--- ajax/add_form_result.php
+++ ajax/add_form_result.php
@@ -1,17 +1,25 @@
 <?php
+
 header('Content-Type: application/json; charset=utf-8');
 
 require_once $_SERVER["DOCUMENT_ROOT"] . "/app/include.php";
 
 $validator = new Validator();
-
 $validatorData = new ValidatorData();
 
-$validatorData->name = $_POST["name"];
-$validatorData->secondName = $_POST["second-name"];
-$validatorData->email = $_POST["email"];
-$validatorData->password = $_POST["password"];
-$validatorData->passwordConfirm = $_POST["password-confirm"];
+try {
+    $validatorData->name = $_POST["formData"]["name"];
+    $validatorData->secondName = $_POST["formData"]["secondName"];
+    $validatorData->email = $_POST["formData"]["email"];
+    $validatorData->password = $_POST["formData"]["password"];
+    $validatorData->passwordConfirm = $_POST["formData"]["passwordConfirm"];
+} catch (Exception|Error) {
+    json_encode([
+        "success" => false,
+        "message" => "empty data"
+    ], JSON_UNESCAPED_UNICODE);
+    die();
+}
 
 $response = $validator->validate($validatorData);
 
